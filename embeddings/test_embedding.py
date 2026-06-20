fde0e628fa5a62b29f820851317517125f352dc6
Maheshwari Rewatkar <rewatkarmaheshwari@gmail.com>
2026-06-20 14:43:48 +0530

    Added text cleaning and chunking module for document preprocessing

---
+++ embeddings/test_embedding.py
@@ -0,0 +1,50 @@
+from text_processing.cleaner import TextCleaner
+from text_processing.chunker import TextChunker
+
+from embeddings.embedding_model import (
+    EmbeddingGenerator
+)
+
+with open(
+    "sample.txt",
+    "r",
+    encoding="utf-8"
+) as file:
+
+    text = file.read()
+
+clean_text = TextCleaner.clean_text(text)
+
+chunks = TextChunker.chunk_text(
+    clean_text,
+    chunk_size=500
+)
+
+generator = EmbeddingGenerator()
+
+embeddings = generator.generate_embeddings(
+    chunks
+)
+
+print(
+    "Total Chunks:",
+    len(chunks)
+)
+
+print(
+    "Total Embeddings:",
+    len(embeddings)
+)
+
+print(
+    "Embedding Dimension:",
+    len(embeddings[0])
+)
+
+print(
+    "\nFirst 10 Values:\n"
+)
+
+print(
+    embeddings[0][:10]
+)
---
+++ text_processing/cleaner.py
@@ -0,0 +1,11 @@
+import re
+
+class TextCleaner:
+
+    @staticmethod
+    def clean_text(text):
+
+        text = re.sub(r'\s+', ' ', text)
+        text = text.strip()
+
+        return text
